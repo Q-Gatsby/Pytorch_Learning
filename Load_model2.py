fb6b591895ff97ed29ed130fa6d1a29ecbb6a340
Q-Gatsby <97495112+Q-Gatsby@users.noreply.github.com>
2022-09-13 11:44:00 +0800

    Delete Load_model2.py

--- Load_model2.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torchvision
-
-# vgg16 = torch.load('vgg16_method2.pth')
-# print(vgg16)   # 此时单独去加载模型，出来的是字典格式的模型
-
-vgg16 = torchvision.models.vgg16(pretrained=False)
-vgg16.load_state_dict(torch.load('vgg16_method2.pth'))
-print(vgg16)
